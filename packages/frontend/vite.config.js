fbfba7eca8b9a9685e2501780fe3f5b9c76a575b
Filipe-Matos-Viriato <filipematos@viriato.com.pt>
2025-07-21 15:46:30 +0100

    Filipe branch

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js', // Explicitly point to postcss.config.js
-  },
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
     port: 5174,      // Explicitly set the port to 5174
